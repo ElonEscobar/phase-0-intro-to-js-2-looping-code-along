8586fa2ca29de3933e1c84e88dcd927b8c85cbf5
Dan Muendo <danwalker254254@gmail.com>
2022-08-18 12:02:19 +0100

    Assignment done

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 // Code your solutions in this file
-/*
-!this code is from the code-along
-for (age = 30; age < 40; age++){
-    console.log(`I'm ${age} years old. Happy birthday to me!\n`);
-}
-
-const gifts = ["teddy bear","drone","doll"];
-function wrapGifts(gifts){
-    for(let i = 0; i < gifts.length; i++){
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
-        debugger;
-    }
-    return gifts;
-}
-wrapGifts(gifts);
-*/
-
-//solution to the assignment
-
 function writeCards(name , eventName){
     const writeCardsArray = [];
     for(let x=0; x < name.length; x++){
